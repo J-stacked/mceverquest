fe417e72e5c11ef39cb474b97abb0620a5c50373
J-stacked <146044161+J-stacked@users.noreply.github.com>
2024-01-04 17:41:15 -0500

    Names are figured out!
    Issue closed.

--- src/main/java/net/mceq/mceverquest/MCEverQuest.java
+++ src/main/java/net/mceq/mceverquest/MCEverQuest.java
@@ -3,9 +3,16 @@ package net.mceq.mceverquest;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.mceq.mceverquest.entity.ModEntities;
 import net.mceq.mceverquest.entity.custom.FireelementalEntity;
 import net.mceq.mceverquest.entity.custom.RatEntity;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
